d303f1725c94eaf231e8b05e3fd2b37766e7f9f3
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-05-19 03:09:59 -0400

    Replace printf with println for printing

--- GAME/DER/fsst8.java
+++ GAME/DER/fsst8.java
@@ -4,30 +4,30 @@ public class fsst8 {
 	public static void main(String[] args) throws InterruptedException {
 		Scanner Input = new Scanner(System.in);
 		
-		System.out.printf("Fire: Short Story 8\n");
+		System.out.println("Fire: Short Story 8");
 		Thread.sleep(1000);
-		System.out.printf("This is an interactive test program for JAVA\n");
+		System.out.println("This is an interactive test program for JAVA");
 		Thread.sleep(1000);
-		System.out.printf("I AM COMPLETELY NEW TO JAVA\n");
+		System.out.println("I AM COMPLETELY NEW TO JAVA");
 		Thread.sleep(1000);
-		System.out.printf("Copyright under the MIT License\n\n");
+		System.out.println("Copyright under the MIT License\n");
 		Thread.sleep(1000);
-		System.out.printf("You wake up from a nap in the tent. It is night, owls are hooting, the birds are asleep... You smell smoke, you see a fire!\n");
+		System.out.println("You wake up from a nap in the tent. It is night, owls are hooting, the birds are asleep... You smell smoke, you see a fire!");
 		Thread.sleep(1000);
-		System.out.printf("1. Grab bottles of water and pour them into a bucket and throw it onto the fire | 2. Watch it burn everything\n");
+		System.out.println("1. Grab bottles of water and pour them into a bucket and throw it onto the fire | 2. Watch it burn everything");
 		System.out.printf("CHOICE: ");
 		int a = Input.nextInt();
 		
 		if (a == 1) {
 			Thread.sleep(1000);
-			System.out.printf("\nYou attempt to grab a bunch of water bottles, the fire ingulfs you...\n");
+			System.out.println("\nYou attempt to grab a bunch of water bottles, the fire ingulfs you...\n");
 		}
 		else if (a == 2) {
 			Thread.sleep(1000);
-			System.out.printf("\nYou watch the fire ingulf everything... You notice a figure watching.\n");
+			System.out.println("\nYou watch the fire ingulf everything... You notice a figure watching.\n");
 		}
 		else {
-			System.out.printf("\nINVALID.\n\n");
+			System.out.println("\nINVALID.\n");
 		}
 	}
 }
